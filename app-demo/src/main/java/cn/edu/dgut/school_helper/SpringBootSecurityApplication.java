f28f0536307cd559a0f72f350bf4c02f74a0e378
xtakeit <956064633@qq.com>
2020-03-11 15:01:51 +0800

    把ide改为idea

--- app-demo/src/main/java/cn/edu/dgut/school_helper/SpringBootSecurityApplication.java
+++ app-demo/src/main/java/cn/edu/dgut/school_helper/SpringBootSecurityApplication.java
@@ -3,12 +3,10 @@ package cn.edu.dgut.school_helper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScan;
 
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication  
-@ComponentScan(basePackages= {"cn.edu.dgut.school_helper"})
+@SpringBootApplication
 @MapperScan(basePackages= {"cn.edu.dgut.school_helper.**.mapper"})
 @ServletComponentScan
 public class SpringBootSecurityApplication {
